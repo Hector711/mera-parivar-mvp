df39226ef78ea60a7c1de5ef407726e7145457df
Iván <ivaninmenendez@gmail.com>
2024-04-03 20:34:13 +0200

    test(BACKEND): :memo: estamos haciendo pruebas, no os asusteis

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
+  res.json({ message: 'Hello World!!!!!' });
 });
 
 module.exports = router;
